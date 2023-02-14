83410579fc461f23946202e97871ca71eae1a007
amnganga <amnganga@safaricom.co.ke>
2023-02-14 13:27:22 +0300

    updated the hikari db url

--- src/main/kotlin/com/example/db/DataBaseFactory.kt
+++ src/main/kotlin/com/example/db/DataBaseFactory.kt
@@ -32,7 +32,7 @@ object DataBaseFactory {
         config.maximumPoolSize = 3
         config.isAutoCommit = false
         config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
-        config.validate()
+
 //        config.jdbcUrl= "jdbc:postgresql://knjhdahpscmuxl:51b46fd9f307eda51a2340dd297b4fced1431ff2511893c632534928c87288cd@ec2-44-199-143-43.compute-1.amazonaws.com:5432/d5ovidmele2ehi?user=t&password=1234"
 
         val uri = URI(System.getenv("DATABASE_URL"))
@@ -42,6 +42,7 @@ object DataBaseFactory {
         config.jdbcUrl =
             "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?sslmode=require" + "&user=$username&password=$password"
 
+        config.validate()
 
         return HikariDataSource(config)
     }
