89cd1391831d12c218a5c042a2545f6d1a185f7a
amnganga <amnganga@safaricom.co.ke>
2023-02-14 14:15:51 +0300

    Change db URL

--- src/main/kotlin/com/example/db/DataBaseFactory.kt
+++ src/main/kotlin/com/example/db/DataBaseFactory.kt
@@ -27,21 +27,15 @@ object DataBaseFactory {
 
     private fun hikari(): HikariDataSource{
         val config = HikariConfig()
-        config.driverClassName = System.getenv("JDBC_DRIVER")
-//        config.jdbcUrl= System.getenv("DATABASE_URL")
-        config.maximumPoolSize = 3
-        config.isAutoCommit = false
-        config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
-
-//        config.jdbcUrl= "jdbc:postgresql://knjhdahpscmuxl:51b46fd9f307eda51a2340dd297b4fced1431ff2511893c632534928c87288cd@ec2-44-199-143-43.compute-1.amazonaws.com:5432/d5ovidmele2ehi?user=t&password=1234"
-
         val uri = URI(System.getenv("DATABASE_URL"))
         val username = uri.userInfo.split(":").toTypedArray()[0]
         val password = uri.userInfo.split(":").toTypedArray()[1]
 
-        config.jdbcUrl =
-            "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?sslmode=require" + "&user=$username&password=$password"
-
+        config.driverClassName = System.getenv("JDBC_DRIVER")
+        config.jdbcUrl = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?sslmode=require" + "&user=$username&password=$password"
+        config.maximumPoolSize = 3
+        config.isAutoCommit = false
+        config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
         config.validate()
 
         return HikariDataSource(config)
